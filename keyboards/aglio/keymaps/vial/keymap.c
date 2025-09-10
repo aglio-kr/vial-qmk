328a65f107cb9ce82182cf2b8751452e1d7c399c
Seungje Ryu <seungje.ryu@gmail.com>
2025-09-10 13:40:22 +0900

    Update keymap.c

--- keyboards/aglio/keymaps/vial/keymap.c
+++ keyboards/aglio/keymaps/vial/keymap.c
@@ -37,12 +37,3 @@ bool led_update_user(led_t led_state) {
   }      
   return true;
 }
-
-const uint16_t PROGMEM key_combos[0] = {
-};
-
-const td_data_t tap_dance_actions[] = {
-};
-
-const key_override_t key_overrides[] = {
-};
